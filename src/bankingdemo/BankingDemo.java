da7b15b34da34e25db02b7e5dcaca141bf802867
Ashika Shallow <slwashika@gmail.com>
2020-08-07 10:31:28 -0400

    Added boolean to control while loop

--- src/bankingdemo/BankingDemo.java
+++ src/bankingdemo/BankingDemo.java
@@ -1,8 +1,10 @@
-
 package bankingdemo;
+
+import java.util.Scanner;
 
 /**
  * This program demonstrates abstract classes
+ *
  * @author Ashika Shallow
  */
 public class BankingDemo {
@@ -11,7 +13,37 @@ public class BankingDemo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
+        int choice;
+        double amount;
+        boolean keepGoing = true;
+        Scanner kb = new Scanner(System.in);
+
+        SavingsAccount account = new SavingsAccount(65.14, 0.13);
+
+        System.out.println("Your account balance is: $" + account.getBalance());
+
+        while (keepGoing) {
+            System.out.print("What would you like to do? withdraw(1), deposit(2), finish(0) ");
+            choice = kb.nextInt();
+
+            if (choice == 1) {
+                System.out.print("Amount to withdraw: ");
+                amount = kb.nextDouble();
+                account.withdraw(amount);
+            } else if(choice == 2){
+                System.out.print("Amount to deposit: ");
+                amount = kb.nextDouble();
+                account.deposit(amount);
+            } else{
+                keepGoing = false;
+            }            
+
+        }
+
+        account.monthlyProcess();
+
+        System.out.println("Your new balance is: " + account.getBalance());
     }
-    
+
 }
